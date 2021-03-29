8d0fa348ea54a11e18350cb42f4f94a337e03d6b
Job Feikens <job@vv32.nl>
2021-03-29 15:54:06 +0200

    Optimized codec

--- src/main/java/nl/vv32/rcon/PacketCodec.java
+++ src/main/java/nl/vv32/rcon/PacketCodec.java
@@ -1,34 +1,36 @@
 package nl.vv32.rcon;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class PacketCodec {
+
+    final private static Charset CHARSET = StandardCharsets.US_ASCII;
 
     public static void encode(final Packet packet, final ByteBuffer destination) {
 
         destination.putInt(packet.requestId);
         destination.putInt(packet.type.id);
-        destination.put(StandardCharsets.US_ASCII.encode(packet.payload));
+        destination.put(CHARSET.encode(packet.payload));
         destination.put((byte) 0x00);
         destination.put((byte) 0x00);
     }
 
     public static Packet decode(final ByteBuffer source, final int length) {
+        int requestId = source.getInt();
 
-        int startPos = source.position();
-
-        final int requestId = source.getInt();
-        final PacketType type = PacketType.fromId(source.getInt());
+        PacketType type = PacketType.fromId(source.getInt());
 
         int limit = source.limit();
-        source.limit(startPos + length - 2);
-
-        final String payload = StandardCharsets.US_ASCII.decode(source).toString();
+        source.limit(source.position() + length - 10);
+        String payload = CHARSET.decode(source).toString();
         source.limit(limit);
 
-        source.get();
-        source.get();
+        source.get(); // String termination
+        source.get(); // Packet termination
 
         return new Packet(requestId, type, payload);
     }
